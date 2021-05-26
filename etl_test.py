67b1386e70c82f15231d159be3696da0db1dc207
bushan34 <74500765+bushan34@users.noreply.github.com>
2021-05-26 13:57:07 +0300

    Update and rename etl_1.py to etl_test.py

--- etl_test.py
+++ etl_test.py
@@ -2,7 +2,6 @@ from datetime import timedelta, datetime
 from random import randint
 
 from airflow import DAG
-#from airflow.contrib.operators.dataproc_operator import DataProcHiveOperator
 from airflow.operators.postgres_operator import PostgresOperator
 #from airflow.operators.python_operator import PythonOperator
 #SQL Scripts
@@ -37,37 +36,10 @@ default_args = {
 }
 
 dag = DAG(
-    USERNAME + '.etl_1',
+    USERNAME + '.etl_test',
     default_args=default_args,
     description='ETL tasks project',
     schedule_interval="0 0 1 1 *",
 	concurrency=1,
     max_active_runs=1,
 )
-
-#tables = ('payment', 'mdm')
-#ods = []
-#for table in tables:
-#    if table == 'payment':
-#        query = """
-#            insert overwrite table nnaranov.pro_ods_payment partition (year='{{ execution_date.year }}') 
-#            select user_id, pay_doc_type, cast(pay_doc_num as INT), account, phone, billing_period, cast(pay_date as DATE), cast(sum as DECIMAL(8,2))
-#            from nnaranov.stg_payment where year(pay_date) = {{ execution_date.year }};
-#            """
-#    elif table == 'mdm':
-#        query = """
-#            insert overwrite table nnaranov.pro_ods_mdm partition (year='{{ execution_date.year }}') 
-#            select id, district, registered_at, billing_mode, is_vip
-#            from mdm.user where year(from_unixtime(registered_at div 1000)) = {{ execution_date.year }};
-#            """
-
-#    ods = DataProcHiveOperator(
-#        task_id='pro_ods_' + table,
-#        dag=dag,
-#        query=query,
-#        cluster_name='cluster-dataproc',
-#        job_name=USERNAME + 'pro_ods_' + table + '_{{ execution_date.year }}_{{ params.job_suffix }}',
-#       params={"job_suffix": randint(0, 100000)},
-#        region='europe-west3',
-#    )
-#ods
