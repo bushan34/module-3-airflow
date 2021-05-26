7c187c15f7abdb5712d1f3a075b1e96951b0de21
bushan34 <74500765+bushan34@users.noreply.github.com>
2021-05-26 18:52:06 +0300

    Update dwh_report_dm_project.py

--- dwh_report_dm_project.py
+++ dwh_report_dm_project.py
@@ -98,7 +98,7 @@ username = 'nnaranov'
 default_args = {
     'owner': username,
     'depends_on_past': False,
-    'start_date': datetime(2013, 1, 1, 0, 0, 0),
+    'start_date': datetime(2010, 1, 1, 0, 0, 0),
     'email': ['airflow@example.com'],
     'email_on_failure': False,
     'email_on_retry': False,
